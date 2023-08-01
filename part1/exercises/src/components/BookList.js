edd79f9df1aa33e1028191a71c4e4d9f2f4aa557
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-07-31 22:30:29 -0500

    component bookslist added

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,17 @@
+import classes from './BookList.module.css'
+
 export default function BookList() {
    let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let book1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAci3zcqGMxzJpUkwyD8NUCaqzA975zWxf2g&usqp=CAU";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFhfiPduI5kq9E5QWsAf1chi3FBjmUzT2tRA&usqp=CAU";
+   let book3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtmfOFoxe8Dqnf5UaNMwk16r76Kn08frbcdQ&usqp=CAU";
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <h3 className = {classes.bookHeading}>{pageTitle}</h3>
+         <img src={book1} alt="choose to run" />
+         <img src={book2} alt="Michelle Obama" />
+         <img src={book3} alt="The Hobbit" />
       </div>      
    );
 }
