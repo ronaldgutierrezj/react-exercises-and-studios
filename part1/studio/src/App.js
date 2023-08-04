68397f7054237d44bad96580222f72cd1d08d17f
Ronald Gutierrez <ronaldgutierrezj@gmail.com>
2023-08-04 10:39:15 -0500

    finished: aligned of div completed

--- part1/studio/src/App.js
+++ part1/studio/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import RecipeDescription from './components/Description';
 import RecipeIngredients from './components/Ingredients'
 import RecipePhoto from './components/Photos';
-import styles from './components/Description.module.css';
+
 
 function App() {
   return (
     <div className="App">
-      <div className={styles.recipePhotoBlock}>
+      <div className="recipePhotoBlock">
         <RecipePhoto />
         <div>
           <RecipeDescription />
